d3a5decad1f16aaea5281d6ebb798b621be7f979
Madnarth <corwyn.blade@gmail.com>
2018-05-15 15:45:47 +0200

    Initial commit

---
+++ Czat Serwer/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Czat_Serwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxAdmin = new System.Windows.Forms.GroupBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listBoxServer = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownPort = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxIpAddress = new System.Windows.Forms.ComboBox();
+            this.webBrowserChat = new System.Windows.Forms.WebBrowser();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.backgroundWorkerMainLoop = new System.ComponentModel.BackgroundWorker();
+            this.groupBoxAdmin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxAdmin
+            // 
+            this.groupBoxAdmin.Controls.Add(this.buttonRemove);
+            this.groupBoxAdmin.Controls.Add(this.label3);
+            this.groupBoxAdmin.Controls.Add(this.listBoxUsers);
+            this.groupBoxAdmin.Controls.Add(this.buttonStop);
+            this.groupBoxAdmin.Controls.Add(this.buttonStart);
+            this.groupBoxAdmin.Controls.Add(this.listBoxServer);
+            this.groupBoxAdmin.Controls.Add(this.label2);
+            this.groupBoxAdmin.Controls.Add(this.label1);
+            this.groupBoxAdmin.Controls.Add(this.numericUpDownPort);
+            this.groupBoxAdmin.Controls.Add(this.comboBoxIpAddress);
+            this.groupBoxAdmin.Location = new System.Drawing.Point(23, 12);
+            this.groupBoxAdmin.Name = "groupBoxAdmin";
+            this.groupBoxAdmin.Size = new System.Drawing.Size(250, 348);
+            this.groupBoxAdmin.TabIndex = 0;
+            this.groupBoxAdmin.TabStop = false;
+            this.groupBoxAdmin.Text = "Panel Administracyjny";
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(7, 315);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 9;
+            this.buttonRemove.Text = "Usuń";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Lista użytkowników:";
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.Location = new System.Drawing.Point(124, 204);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(120, 134);
+            this.listBoxUsers.TabIndex = 7;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(167, 175);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(7, 175);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listBoxServer
+            // 
+            this.listBoxServer.FormattingEnabled = true;
+            this.listBoxServer.Location = new System.Drawing.Point(6, 73);
+            this.listBoxServer.Name = "listBoxServer";
+            this.listBoxServer.Size = new System.Drawing.Size(237, 95);
+            this.listBoxServer.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adres IP:";
+            // 
+            // numericUpDownPort
+            // 
+            this.numericUpDownPort.Location = new System.Drawing.Point(123, 47);
+            this.numericUpDownPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDownPort.Name = "numericUpDownPort";
+            this.numericUpDownPort.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownPort.TabIndex = 1;
+            this.numericUpDownPort.Value = new decimal(new int[] {
+            25000,
+            0,
+            0,
+            0});
+            // 
+            // comboBoxIpAddress
+            // 
+            this.comboBoxIpAddress.FormattingEnabled = true;
+            this.comboBoxIpAddress.Location = new System.Drawing.Point(123, 19);
+            this.comboBoxIpAddress.Name = "comboBoxIpAddress";
+            this.comboBoxIpAddress.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxIpAddress.TabIndex = 0;
+            this.comboBoxIpAddress.Text = "127.0.0.1";
+            // 
+            // webBrowserChat
+            // 
+            this.webBrowserChat.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowserChat.Location = new System.Drawing.Point(279, 12);
+            this.webBrowserChat.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserChat.Name = "webBrowserChat";
+            this.webBrowserChat.Size = new System.Drawing.Size(393, 348);
+            this.webBrowserChat.TabIndex = 1;
+            this.webBrowserChat.Url = new System.Uri("about:blank", System.UriKind.Absolute);
+            this.webBrowserChat.WebBrowserShortcutsEnabled = false;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(30, 367);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Wyślij";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(111, 370);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(561, 20);
+            this.textBoxMessage.TabIndex = 3;
+            this.textBoxMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMessage_KeyDown);
+            // 
+            // backgroundWorkerMainLoop
+            // 
+            this.backgroundWorkerMainLoop.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerMainLoop_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 411);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.webBrowserChat);
+            this.Controls.Add(this.groupBoxAdmin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxAdmin.ResumeLayout(false);
+            this.groupBoxAdmin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxAdmin;
+        private System.Windows.Forms.NumericUpDown numericUpDownPort;
+        private System.Windows.Forms.ComboBox comboBoxIpAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listBoxServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.WebBrowser webBrowserChat;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerMainLoop;
+    }
+}
+
